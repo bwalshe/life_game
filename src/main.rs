b2365da9968142afbd83550ad0e774f3489879d8
Brian Walshe <walshe.brian@gmail.com>
2018-08-05 18:00:02 +0100

    Code format changes

--- src/main.rs
+++ src/main.rs
@@ -19,28 +19,49 @@ const P_BRAIN: f32 = 0.01;
 pub fn main() {
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
-    let window = video_subsystem.window("Brian's Brain", SCREEN_WIDTH, SCREEN_HEIGHT)
+    let window = video_subsystem.window(
+        "Brian's Brain", 
+        SCREEN_WIDTH, 
+        SCREEN_HEIGHT)
         .position_centered()
         .build()
         .unwrap();
     let mut canvas = window.into_canvas().build().unwrap();
     let texture_creator = canvas.texture_creator();
 
-    let brain_board = BrainBoard::new_random(BOARD_SIZE, BOARD_SIZE, P_BRAIN).unwrap();
-    let life_board = LifeBoard::new_random(BOARD_SIZE, BOARD_SIZE, P_LIFE).unwrap();
+    let brain_board = BrainBoard::new_random(BOARD_SIZE, 
+                                             BOARD_SIZE, 
+                                             P_BRAIN).unwrap();
+    let life_board = LifeBoard::new_random(BOARD_SIZE, 
+                                           BOARD_SIZE, 
+                                           P_LIFE).unwrap();
     {
-    let mut renderer = AutomataRenderer::new(&sdl_context, &mut canvas, &texture_creator, brain_board, FRAMES_PER_SECOND, brain_color).unwrap();
-    let (frames, duration) = renderer.run().unwrap();
-    println!("Ran {} frames in {}.{} seconds", frames, duration.as_secs(), duration.subsec_millis());
-    println!("That's {} frames per second.", 
-        frames as f32 / (duration.as_secs() as f32 + duration.subsec_millis() as f32 / 1000.0));
+        let mut renderer = AutomataRenderer::new(&sdl_context, 
+                                                 &mut canvas, 
+                                                 &texture_creator, 
+                                                 brain_board, 
+                                                 FRAMES_PER_SECOND, 
+                                                 brain_color).unwrap();
+        let (frames, duration) = renderer.run().unwrap();
+        println!("Ran {} frames in {}.{} seconds", frames, duration.as_secs(),
+                 duration.subsec_millis());
+        println!("That's {} frames per second.", 
+            frames as f32 / (duration.as_secs() as f32 
+                           + duration.subsec_millis() as f32 / 1000.0));
     }
     {
-    let mut renderer = AutomataRenderer::new(&sdl_context, &mut canvas, &texture_creator, life_board, FRAMES_PER_SECOND, life_color).unwrap();
-    let (frames, duration) = renderer.run().unwrap();
-    println!("Ran {} frames in {}.{} seconds", frames, duration.as_secs(), duration.subsec_millis());
-    println!("That's {} frames per second.", 
-        frames as f32 / (duration.as_secs() as f32 + duration.subsec_millis() as f32 / 1000.0));
+        let mut renderer = AutomataRenderer::new(&sdl_context, 
+                                                 &mut canvas, 
+                                                 &texture_creator, 
+                                                 life_board, 
+                                                 FRAMES_PER_SECOND, 
+                                                 life_color).unwrap();
+        let (frames, duration) = renderer.run().unwrap();
+        println!("Ran {} frames in {}.{} seconds", frames, duration.as_secs(), 
+                 duration.subsec_millis());
+        println!("That's {} frames per second.", 
+            frames as f32 / (duration.as_secs() as f32 
+                           + duration.subsec_millis() as f32 / 1000.0));
     }
 }
 
